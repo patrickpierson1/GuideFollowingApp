2cc7a7af4a88989f006b77ed4302aa5c3c563098
msvining <marsvining@gmail.com>
2026-05-12 21:24:00 -0700

    add setup script

---
+++ backend/setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+mkdir temp
+export TMPDIR=$PWD/temp
+
+#sudo apt update && sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev git libbz2-dev liblzma-dev
+#wget https://www.python.org/ftp/python/3.11.4/Python-3.11.4.tgz
+#tar xzvf Python-3.11.4.tgz 
+cd Python-3.11.4/
+#./configure --with-default-suites=openssl
+#sudo make altinstall
+
+cd ..
+
+sudo rm /usr/bin/python3
+sudo ln -s /usr/local/bin/python3.11 /usr/bin/python3
+sudo rm /usr/bin/pip
+sudo rm /usr/bin/pip3
+sudo ln -s /usr/local/bin/pip3.11 /usr/bin/pip3
+sudo ln -s /usr/local/bin/pip3.11 /usr/bin/pip
+
+python3 -m venv env
+python -m pip install --upgrade pip setuptools wheel
+source env/bin/activate
+pip install -r requirements.txt
